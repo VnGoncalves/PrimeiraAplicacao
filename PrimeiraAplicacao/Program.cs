96ec5588527f91086e81fd131056e7762467b5e7
Vinicius Nascimento <vinicius_1571@outlook.com>
2024-11-03 18:46:43 -0300

    Implementando mais funções ao console.

--- PrimeiraAplicacao/Program.cs
+++ PrimeiraAplicacao/Program.cs
@@ -1,23 +1,28 @@
-﻿
-string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+﻿string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
+List<string> ListaDasBandas = new List<string> { "U2", "Beatles", "Callypso"};
 
-void ExibirMensagemDeBoasVindas()
+void ExibirLogo()
 {
     Console.WriteLine(@"
-                        ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-                        ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-                        ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-                        ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-                        ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-                        ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+
+");
+
 
     Console.WriteLine(mensagemDeBoasVindas);
-
 }
 
 void ExibirOpcoesDoMenu()
 {
+    ExibirLogo();
+
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -28,12 +33,50 @@ void ExibirOpcoesDoMenu()
     string opcaoEscolhida = Console.ReadLine()!;
     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
 
-    if (opcaoEscolhidaNumerica == 1)
+    switch (opcaoEscolhidaNumerica)
     {
-        Console.WriteLine("Vocë digitou a opção: " + opcaoEscolhida);
+        case 1: RegistrarBanda();
+            break;
+        case 2: MostrarBandasRegistradas();
+            break;
+        case 3: Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhidaNumerica);
+            break;
+        case 4: Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhidaNumerica);
+            break;
+        case -1: Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhidaNumerica);
+            break;
+        default :Console.WriteLine("opcao invalida ");
+            break;
     }
-
 }
 
-ExibirMensagemDeBoasVindas();
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Regsitro de bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    ListaDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    Console.WriteLine("Exibindo todas as bandas registradas");
+
+    for (int i = 0; i < ListaDasBandas.Count; i++)
+    {
+        Console.WriteLine($"Banda: {ListaDasBandas[i]}");
+    }
+
+    Console.WriteLine("Digite uma tecla para voltar ao menu.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
 ExibirOpcoesDoMenu();
